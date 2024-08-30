46f2a017ea298f51514e01b293076e62afd03e2b
Dionisio-ReimonParis <dionisio.rparis00133@dyci.edu.ph>
2024-08-31 02:18:13 +0800

    Add files via upload

---
+++ hands-on1-3c/src/component/Description/Description.jsx
@@ -0,0 +1,11 @@
+import './Description.css';
+function Description({description}) {
+    return (
+        <div>
+            <p className='text-c1c1c1'>{description}</p>
+        </div>
+    )
+    
+}
+
+export default Description;
